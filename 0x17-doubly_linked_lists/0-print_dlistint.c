e618282f03f932430c839942180fcd788001d9d3
Kingsley97Dade <kingsleydade@gmail.com>
2023-11-09 03:42:26 -0800

    Tasks on Double linked lists in C

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dll
+ * @h: head of doubly linked list
+ * Return: - number of nodes in the doubly linked list
+ *
+ * AUTHOR - Kingsley Dade
+ */
+size_t print_dlistint(const dlistint_t *h);
+{
+	const dlistint_t *current = h;
+	int num_of_nodes = 0;
+
+	/* if head passed is NULL */
+	if (h == NULL)
+		return (0);
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		num_of_nodes++;
+	}
+	return (num_of_nodes);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,5 @@
+<h1>0x17. C - Doubly linked lists</h1>
+<p>What is a doubly linked list</p>
+<p>How to use doubly linked lists</p>
+<p>Understand and know how to implement the various operations (deletion, insertion, etc) with doubly linked lists</p>
+<p>Start to look for the right source of information without too much help</p>
